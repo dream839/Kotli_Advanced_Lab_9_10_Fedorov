19008d61dc978e5a67c4c9daca3a3571414f2578
dream839 <mihailfedorov3973@gmail.com>
2026-02-26 19:50:44 +0300

    Add data class OutpostResource and ResourceManager

---
+++ src/OutpostResource.kt
@@ -0,0 +1,25 @@
+package resources
+
+import kotlin.properties.Delegates
+
+data class OutpostResourse(
+    val id: Int,
+    val name: String,
+    var amountInit: Int
+) {
+    var amount: Int by Delegates.observable(amountInit){
+            _, old, new ->
+        println("Ресурс [$name] изменился: $old -> $new")
+    }
+}
+
+fun main(){
+    val gas = OutpostResourse(1,"Gas", 100)
+    val mineral = OutpostResourse(2, "Minerals", 250)
+    println("Успех! Вы добавили доп. кол-во минералов: " +
+            "${mineral.amount + 50}")
+    val bonusMineral = mineral.copy(3,"Mineral Bonus", amountInit = mineral.amount + 50)
+    println(gas.toString())
+    println(mineral.toString())
+    println(bonusMineral.toString())
+}
--- src/Test.kt
+++ src/Test.kt
@@ -1,12 +1,23 @@
+package example
+
 var age: Int = 18
     set(value) {
         if ((value > 0) and (value < 110))
             field = value
     }
-fun main() {
-    println(age)
-    age=45
-    println(age)
-    age = -345
-    println(age)
+//fun main() {
+//    println(example.age)
+//    example.age = 45
+//    println(example.age)
+//    example.age = -345
+//    println(example.age)
+//}
+data class Item(
+    val id: Int,
+    val name: String,
+    val quantity: Int
+){
+    override fun toString(): String {
+        return "Id предмета: $id\nИмя: $name\nКоличество: $quantity\n"
+    }
 }
