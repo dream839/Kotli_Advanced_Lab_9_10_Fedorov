9dfce6775dc9602e0f5d998fe3ec764369fb3baf
dream839 <mihailfedorov3973@gmail.com>
2026-02-26 21:59:16 +0300

    learn Object

--- src/Main.kt
+++ src/Main.kt
@@ -15,3 +15,14 @@ fun main() {
 //    manager.printAll()
 //    val bonus = minerals.copy(amount = minerals.amount + 50)
 //    println("опия минералов с бонусами $bonus")
+//    println("Программа запущена")
+//    println("Проверяем состояние, но не трогаем GameSession")
+//    println("Теперь запускаем игру")
+//    GameSession.start()
+//    println("Проверяем состояние ещё раз")
+//    println("Активна ли сессия: ${GameSession.isActive}")
+//    Logger.log("Первое сообщение")
+//    Logger.log("Второе сообщение")
+//    val logger1 = Logger
+//    val logger2 = Logger
+//    println(logger1 == logger2)
