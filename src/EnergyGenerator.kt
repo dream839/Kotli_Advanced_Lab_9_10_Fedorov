298d5a31e0765cac8bb718970cdb0407677dfd8c
dream839 <mihailfedorov3973@gmail.com>
2026-02-26 20:20:25 +0300

    Add abstract modules, interfaces andfirst moduleimplementations

---
+++ src/EnergyGenerator.kt
@@ -0,0 +1,12 @@
+package modules
+
+import resources.OutpostResourse
+import resources.ResourceManager
+
+class EnergyGenerator: OutpostModule("Генератор энергии") {
+    override fun performAction(manager: ResourceManager) : ModuleResult {
+        println("Генератор работает... Производит 20 энергии")
+        val energy = manager.get("Energy")
+        return if (energy != null){
+            energy.amount += 20
+            ModuleResult.ResourceProduced("Energy", 20)
---
+++ src/OutpostModule.kt
@@ -0,0 +1,16 @@
+package modules
+
+import resources.ResourceManager
+
+abstract class OutpostModule(
+    val name: String,
+    var level: Int = 1
+) {
+
+    fun upgrade(){
+        level++
+        println("$name улучшен до уровня $level")
+    }
+
+    abstract fun performAction(manager: ResourceManager) : ModuleResult
+}
