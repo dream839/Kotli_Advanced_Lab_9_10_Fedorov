88d5060cc130e2e5e16da9ecb973f81275d36969
dream839 <mihailfedorov3973@gmail.com>
2026-02-27 10:01:36 +0300

    add delegation, lazy, observer and state saving practice

---
+++ src/outpostState_DZ12/ObservableResource.kt
@@ -0,0 +1,16 @@
+package outpostState_DZ12
+
+import outpostState_DZ12.ResourceObserver
+import kotlin.properties.Delegates
+
+class ObservableResource(
+    val name: String,
+    initialAmount: Int
+) {
+    private val observer = ResourceObserver()
+
+    var amount: Int by Delegates.observable(initialAmount) { property, old, new ->
+        println("Ресурс $name изменён: $old → $new")
+        observer.onResourceChanged(name, old, new)
+    }
+}
---
+++ src/outpostState_DZ12/OutpostManager.kt
@@ -0,0 +1,12 @@
+package outpostState_DZ12
+
+class OutpostManager {
+    val resources: List<ObservableResource> by lazy {
+        println("Создание менеджера...")
+        listOf(
+            ObservableResource("Minerals", 100),
+            ObservableResource("Gas", 50),
+            ObservableResource("Energy", 200)
+        )
+    }
+}
